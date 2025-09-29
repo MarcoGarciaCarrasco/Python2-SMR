e85ace421670912cf9ab4111545e0a104d01e94c
Marco García Carrasco <mgarcar0106@g.educaand.es>
2025-09-29 12:39:03 +0200

    Ejercicios python

---
+++ Ejemplos/Ejemplo3-4.py
@@ -0,0 +1,9 @@
+#Ejemplo print
+
+
+nombre = input("Hola! introduce tu nombre: ")
+num1 = int(input("Proporcioname un valor:"))
+num2= int(input("Ahora proporcioname un segundo valor:"))
+
+
+print(f"Hola {nombre} el resultado de sumar los dos números es: {num1 + num2}")  
