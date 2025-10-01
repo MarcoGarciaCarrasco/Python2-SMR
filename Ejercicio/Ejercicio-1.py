91d6444e4ee984db51df537cf2166927f51181d1
Marco García Carrasco <mgarcar0106@g.educaand.es>
2025-10-01 13:46:17 +0200

    Ejercicios

---
+++ Ejercicio/Ejercicio-1.py
@@ -0,0 +1,13 @@
+nombre = "Michael Jordan"
+
+edad = 50
+
+media_puntos = 28,5
+
+activo = False
+
+
+print ("nombre: ", nombre)
+print ("Edad: ", edad)
+print ("Media de puntos/Partido: ", media_puntos)
+print ("Activo: ", activo)
---
+++ Ejercicio/Ejercicio-2.py
@@ -0,0 +1,7 @@
+nombre = input("Introduce tu nombre: ")
+
+dni = input("Introduce tu DNI: ")
+
+edad = input("Introduce tu Edad: ")
+
+print("Hola! " + nombre + ", tu DNI es: " + dni + " y tu edad es de: " + edad + " años")
