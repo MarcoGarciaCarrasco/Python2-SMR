4f58a89e40c36a75c0c3206effc2613b87a86285
Marco García Carrasco <mgarcar0106@g.educaand.es>
2025-10-06 12:05:02 +0200

    Ejercicio 7

--- Ejercicio-7.py
+++ Ejercicio-7.py
@@ -1,4 +1,4 @@
 num1 = int(input("Introduce el primer número: "))
 num2 = int(input("Introduce el segundo número: "))
 
-print(f"Suma:{num1+num2}, resta:{num1-num2}")
+print(f"Suma:{num1+num2}, resta:{num1-num2}, Multiplicación:{num1*num2}, División:{num1/num2}")
