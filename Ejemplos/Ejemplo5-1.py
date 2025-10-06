380213bda4764fd331ca396ac6e076f4b89912b6
Marco García Carrasco <mgarcar0106@g.educaand.es>
2025-10-06 12:45:35 +0200

    Ejemplos 6/10

---
+++ Ejemplos/Ejemplo5-1.py
@@ -0,0 +1,26 @@
+#Ejemplo de estructura de control IF
+
+numero = int(input("Introduce un número: "))
+
+
+if numero == 8:
+    
+    print("Acertaste!!!!!")
+
+numero = int(input("Introduce un número: "))
+
+
+if numero == 8:
+    
+    print("Acertaste!!!!")
+
+numero = int(input("Introduce un número: "))
+
+
+if numero == 8:
+    
+    print("Acertaste!!!!!")
+
+
+
+print ("Final del programa")
